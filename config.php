50c757d6a20b6dbad3a746c092d02c7605eec542
zeroisstart <shenongmings@gmail.com>
2013-11-14 10:59:53 +0800

    1.修改配置和格式化

--- config.php
+++ config.php
@@ -6,14 +6,14 @@ if(!defined('IN_SYS')) {
 //配置参数
 $_SC = array();
 $_SC['dbhost']  		= 'localhost:3306'; //服务器地址
-$_SC['dbuser']  		= 'admin'; //数据库用户
+$_SC['dbuser']  		= 'root'; //数据库用户
 $_SC['dbpw'] 	 		= ''; //数据库密码
 $_SC['dbcharset'] 		= 'utf8'; //字符集
 $_SC['pconnect'] 		= 0; //是否持续连接
 $_SC['dbname']  		= 'weixiao'; //数据库
 $_SC['charset'] 		= 'utf-8'; //页面字符集
 $_SC['tablepre']        = 'dz_';    //表头
-$_SC['site_name']       = '微笑微信';  //站点的名字
+$_SC['site_name']       = 'You know it';  //站点的名字
 $_SC['lang']            = 'zh-cn';
 $_SC['notification_email']='notification@sylai.com'; //推送邮箱的地址，目前暂时只支持QQ企业邮箱
 $_SC['notification_email_pwd']='';   //推送邮箱的密码
